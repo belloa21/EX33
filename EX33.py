5ade99e48d87401685364205f5f6c094c894fcb1
alexr <belloa@hartwick.edu>
2019-10-15 08:38:06 -0400

    Update

--- EX33.py
+++ EX33.py
@@ -6,11 +6,11 @@ numbers = []
 
 while i < 6:
     print(f"At the top i is {i}")
-numbers.append(i)
+    numbers.append(i)
 
-i = i + 1
-print("Numbers now: ", numbers)
-print(f"At the bottom i is {i}")
+    i = i + 1
+    print("Numbers now: ", numbers)
+    print(f"At the bottom i is {i}")
 
 
 print("The numbers: ")
